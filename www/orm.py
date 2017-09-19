b9235a1647bb7716001db00eb608306281c2ebff
HanChanXiaMing <31791527+HanChanXiaMing@users.noreply.github.com>
2017-09-19 16:36:17 +0800

    Update orm.py

--- www/orm.py
+++ www/orm.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from orm import Model, StringField, IntegerField
 #把常用的SELECT、INSERT、UPDATE和DELETE操作用函数封装起来
+
+from asyncio
 
 # 连接池由全局变量__pool存储
 @asyncio.coroutine
@@ -21,3 +21,37 @@ def create_pool(loop, **kw):
         minsize=kw.get('minsize', 1),
         loop=loop
     )
+    
+# 创建Select
+@asyncio.coroutine
+def select(sql, args, size=None):   # size可以决定取几条
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        # SQL语句的占位符是?，而MySQL的占位符是%s
+        # 用参数替换而非字符串拼接可以防止sql注入
+        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            # 传入参数 通过fetchmany()获取最多指定数量的记录
+            rs = yield from cur.fetchmany(size)
+        else:
+            # 传入参数 通过fetchall()获取所有记录
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+# INSERT,UPDATE,DELETE语句,可以定义一个通用函数
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount # 通过rowcount返回结果数
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
